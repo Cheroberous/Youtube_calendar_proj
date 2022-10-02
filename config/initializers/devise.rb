2e32ab40aef8a2b0c4b66186b62b66e95ac6434c
Chero <desilvestri.1895042@studenti.uniroma1.it>
2022-10-02 22:49:37 +0200

    risolti problemi view e campo nome

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,7 +5,19 @@
 # are not: uncommented lines are intended to protect your configuration from
 # breaking changes in upgrades (i.e., in the event that future versions of
 # Devise change the default values for those options).
-#
+class TurboFailureApp < Devise::FailureApp
+  def respond
+    if request_format == :turbo_stream
+      redirect
+    else
+      super
+    end
+  end
+
+  def skip_format?
+    %w(html turbo_stream */*).include? request_format.to_s
+  end
+end
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
@@ -15,7 +27,11 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = 'aa1028c19df1a8c9fd3d3ea38deddc3967afdf4aa0eeed1b825db886239f9bb06dcef011b79cc07f61083537e5755c1057b750aa7c25aa94214c4995e7e57658'
-
+  config.parent_controller = 'TurboDeviseController'
+  config.navigational_formats=['*/*', :html,:turbo_stream]
+  config.warden do |manager|
+    manager.failure_app = TurboFailureApp
+  end
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users 
+ 
   get 'login/base'  # porta a app/views/login/base.html.erb
   get "/homepage", to: "homepage#cliente"
+
+  # root "/homepage#cliente"
+
+  
+  
 
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
