bab42f07cdb684c1533566ff3edaa0d36d861c2c
Matteo Papa <papa.1896945@studenti.uniroma1.it>
2022-10-18 19:23:05 +0200

    Views dell'Homepage, Login/Signup, e inizio del Profilo. (+ alcuni fix)

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/profile' => 'profile#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
