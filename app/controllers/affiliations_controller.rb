956cfdc78629e90889777803d5c54131b865fb0a
Matteo Papa <papa.1896945@studenti.uniroma1.it>
2022-10-25 02:48:13 +0200

    Aggiunta richiesta affiliazione del cliente

---
+++ app/controllers/affiliations_controller.rb
@@ -0,0 +1,74 @@
+class AffiliationsController < ApplicationController
+  before_action :set_affiliation, only: %i[ show edit update destroy ]
+
+  # GET /affiliations or /affiliations.json
+  def index
+    @affiliations = Affiliation.all
+  end
+
+  # GET /affiliations/1 or /affiliations/1.json
+  def show
+  end
+
+  # GET /affiliations/new
+  def new
+    @affiliation = Affiliation.new
+    @cliente= current_user.id
+    @manager= params[:manager]
+  end
+
+  # GET /affiliations/1/edit
+  def edit
+  end
+
+  # POST /affiliations or /affiliations.json
+  def create
+    @affiliation = Affiliation.new(affiliation_params)
+    @cliente= affiliation_params[:cliente]
+    @manager= affiliation_params[:manager]
+    respond_to do |format|
+      if @affiliation.save
+        format.html { redirect_to "/cliente/managerprofile?id="+@manager}
+        format.json { render :show, status: :created, location: @affiliation }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @affiliation.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /affiliations/1 or /affiliations/1.json
+  def update
+    respond_to do |format|
+      if @affiliation.update(affiliation_params)
+        format.html { redirect_to affiliation_url(@affiliation), notice: "Affiliation was successfully updated." }
+        format.json { render :show, status: :ok, location: @affiliation }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @affiliation.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /affiliations/1 or /affiliations/1.json
+  def destroy
+    @manager=@affiliation.manager
+    @affiliation.destroy
+
+    respond_to do |format|
+      format.html { redirect_to "/cliente/managerprofile?id="+@manager }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_affiliation
+      @affiliation = Affiliation.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def affiliation_params
+      params.require(:affiliation).permit(:cliente, :manager, :status)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do   
+  resources :affiliations
   resources :reviews
   root 'pages#home'
   
