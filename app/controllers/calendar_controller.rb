9af83186d39d91f3f32d95c8e9f3dbbf0555063b
Pippinu <iacono760@gmail.com>
2022-10-26 11:25:58 +0200

    Ok,forse,refresh token e aggiunto Elimina Calendar

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -19,16 +19,15 @@ class CalendarController < ApplicationController
 
         calendarList.items.each do |calendar|
             if calendar.summary === "MMY_USER_#{hash}"
-                # CONTROLLARE SE calendarID sono uguali 
-                if !Calendar.exists?(hash: hash)
-                    calendarToSave = newCalendar(calendar, userID, hash)
-                    # calendarToSave = Calendar.new(
-                    #     calendarId: calendar.id,
-                    #     summary: calendar.summary,
-                    #     managerId: current_user.id,
-                    #     userId: userID,
-                    #     hash: hash.to_s
-                    # )
+                # CONTROLLARE SE calendarID sono uguali
+
+                if !Calendar.exists?(:hash => hash)
+                    foundCalendar = Calendar.where(:hash => hash)
+                    aclCalendarList = client.lists_acls(foundCalendar.calendarId)
+                    acl = aclCalendarList.items.first
+
+                    calendarToSave = newCalendar(calendar, userID, hash, acl.id)
+    
                     # Sistemare, salva ma da errore
                     calendarToSave.save
                 end
@@ -63,13 +62,16 @@ class CalendarController < ApplicationController
         result = client.insert_acl(createdCalendar.id, rule)
 
         # Aggiungo il Calendar appena creato al DB Calendars 
-        calendarToSave = newCalendar(createdCalendar, userID, hash)
+        calendarToSave = newCalendar(createdCalendar, userID, hash, result.id)
         # Da sistemare, salva ma da errore di conversione
         calendarToSave.save
     
         # calendar = Calendar.find_by(hash: hash)
         # redirect_to getCalendar_path(:selectedCalendarId => calendar.id)
         redirect_to manager_path()
+
+    rescue Google::Apis::AuthorizationError
+        rescueUnauthorized(client)
     end
 
     def list_manager_calendar
@@ -100,6 +102,24 @@ class CalendarController < ApplicationController
         @calendar = Calendar.find(selectedCalendar)
     end
 
+    def delete
+        userID = params[:userID]
+
+        client = get_google_calendar_client current_user
+        calendar = Calendar.find_by(managerId: current_user.id, userId: userID)
+
+        if calendar
+            client.delete_acl(calendar.calendarId, calendar.acl_id)
+            client.delete_calendar(calendar.calendarId)
+
+            Calendar.delete(calendar.id)
+        end
+        redirect_to manager_path()
+
+    rescue Google::Apis::AuthorizationError
+        rescueUnauthorized(client)
+    end
+
     def get_google_calendar_client current_user
         client = Google::Apis::CalendarV3::CalendarService.new
         return unless (current_user.present? && current_user.access_token.present? && current_user.refresh_token.present?)
@@ -107,8 +127,8 @@ class CalendarController < ApplicationController
           "web" => {
             "access_token" => current_user.access_token,
             "refresh_token" => current_user.refresh_token,
-            "client_id" => ENV["GOOGLE_API_KEY"],
-            "client_secret" => ENV["GOOGLE_API_SECRET"]
+            "client_id" => ENV["GOOGLE_OAUTH_CLIENT_ID"],
+            "client_secret" => ENV["GOOGLE_OAUTH_CLIENT_SECRET"]
           }
         })
         begin
@@ -139,16 +159,34 @@ class CalendarController < ApplicationController
         return hash.to_s
     end
 
-    def newCalendar(calendar, userID, hash)
+    def newCalendar(calendar, userID, hash, acl_id)
         calendarToSave = Calendar.new(
             calendarId: calendar.id,
             summary: calendar.summary,
             managerId: current_user.id,
             userId: userID,
-            hash: hash.to_s
+            hash: hash.to_s,
+            acl_id: acl_id
         )
 
         return calendarToSave
+    end
+
+    def rescueUnauthorized(client)
+        secrets = Google::APIClient::ClientSecrets.new({
+            "web" => {
+              "access_token" => current_user.access_token,
+              "refresh_token" => current_user.refresh_token,
+              "client_id" => ENV["GOOGLE_OAUTH_CLIENT_ID"],
+              "client_secret" => ENV["GOOGLE_OAUTH_CLIENT_SECRET"]
+            }
+        })
+        client.authorization = secrets.to_authorization
+        client.authorization.grant_type = "refresh_token"
+
+        client.authorization.refresh!
+        current_user.update_attribute(:access_token, client.authorization.access_token)
+        current_user.update_attribute(:refresh_token, client.authorization.refresh_token)
     end
 end
     
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_25_144423) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_26_083726) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -53,6 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_25_144423) do
     t.string "userId"
     t.string "managerId"
     t.string "hash"
+    t.string "acl_id"
   end
 
   create_table "reviews", force: :cascade do |t|
