93e149b53d99428d3ddd9916779b0a79b1a60d3c
Pippinu <iacono760@gmail.com>
2022-10-30 07:25:33 +0100

    aaa

--- app/controllers/youtube_controller.rb
+++ app/controllers/youtube_controller.rb
@@ -5,6 +5,22 @@ class YoutubeController < ApplicationController
     def youtubeListProva
         client = get_google_youtube_client current_user
         @dati = client.list_channels("UCTo-Iq6sT4-tevlofZzid4A")
+    rescue Google::Apis::AuthorizationError
+        secrets = Google::APIClient::ClientSecrets.new({
+            "web" => {
+              "access_token" => current_user.access_token,
+              "refresh_token" => current_user.refresh_token,
+              "client_id" => ENV["GOOGLE_OAUTH_CLIENT_ID"],
+              "client_secret" => ENV["GOOGLE_OAUTH_CLIENT_SECRET"]
+            }
+        })
+        client.authorization = secrets.to_authorization
+        client.authorization.grant_type = "refresh_token"
+
+        client.authorization.refresh!
+        current_user.update_attribute(:access_token, client.authorization.access_token)
+        current_user.update_attribute(:refresh_token, client.authorization.refresh_token)
+        retry
     end
 
     def get_google_youtube_client current_user
